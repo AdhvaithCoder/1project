e2992d4fdd01cdeb2ed738866a1d4f8816f98bc0
AdhvaithCoder <127638263+AdhvaithCoder@users.noreply.github.com>
2023-03-25 19:00:39 +0000

    using image folder

--- project2/project.js
+++ project2/project.js
@@ -1,20 +1,12 @@
 var images = [
-    "https://s3-projectsassets.whjr.online/Monkey_01.png",
-    "https://s3-projectsassets.whjr.online/Monkey_02.png",
-    "https://s3-projectsassets.whjr.online/Monkey_03.png",
-    "https://s3-projectsassets.whjr.online/Monkey_04.png",
-    "https://s3-projectsassets.whjr.online/Monkey_05.png",
-    "https://s3-projectsassets.whjr.online/Monkey_06.png",
-    "https://s3-projectsassets.whjr.online/Monkey_07.png",
-    "https://s3-projectsassets.whjr.online/Monkey_08.png",
-    "https://s3-projectsassets.whjr.online/Monkey_09.png",
-    "https://s3-projectsassets.whjr.online/Monkey_10.png"
+    "images/Monkey_01.png",
+    "images/Monkey_02.png"
     ];
     
     var i=0 ;
     function nextslide() { 
      
-       if(i == 9)
+       if(i == 2)
          {
            i=0;
          }
